7e963b066617821870c7d1a6e041b65b2e8ce661
jean elossy <jean.elossy@yahoo.com>
2022-02-24 17:01:58 +0000

    Initialisation

--- exo_livre/Messages.php
+++ exo_livre/Messages.php
@@ -2,10 +2,11 @@
 
 class Message {
 
+    const LIMIT_USERNAME = 3;
+    const LIMIT_MESSAGE = 10;
     private $username;
     private $message;
-    private $date;
-    private $error;
+    private $date; 
 
 
     public function __construct (string $username, string $message, ?DateTime $date = null) 
@@ -13,16 +14,27 @@ class Message {
         $this->username = $username;
         $this->message = $message;
         $this->date = $date;
-        $this->error = $error;
     }
 
     public function isValid(): bool 
     {
-        if(strlen($this->username) > 3 && strlen($this->message > 10)){
-            
-        }
+        // return strlen($this->username) >= 3 && strlen($this->message >= 10);
+        return empty($this->getErrors());
+    }
+
+    public function getErrors(): array
+    {
+        $errors = [];
         
+        if(strlen($this->username) < self::LIMIT_USERNAME){
+            $errors['username'] = "Votre pseudo trop court";
+        }
+        if(strlen($this->message < self::LIMIT_MESSAGE)){
+            $errors['message'] = "Votre message trop court";
+        }
+        return $errors;
     }
 }
+
 
 ?>
--- exo_livre/index.php
+++ exo_livre/index.php
@@ -1,8 +1,9 @@
 <?php
-require_once 'Message.php';
+require_once 'Messages.php';
 
 $title = "Livre d'or";
-$error = null;
+$errors = null;
+$success = null;
 
 if(isset($_POST['username']) && isset($_POST['message'])){
     // Creer un new message a travers une class Message 
@@ -10,12 +11,10 @@ if(isset($_POST['username']) && isset($_POST['message'])){
 
     // On fait la validation du message a travers une methode isValid()
     if($message->isValid()){
-
+        $success = "Message envoyé !!!";
     }else {
-        $error = "Message non envoyé !!!";
+        $errors = $message->getErrors();
     }
-}else {
-    $error = "Formulaire non envoyé";
 }
 
 
@@ -25,21 +24,38 @@ require_once 'header.php';
 ?>
 
 
-
-<div class="mt-5">   
+<div class="container">   
     <div>
         <h1>Livre d'or</h1>
     </div>
-
-    <?php if($error): ?>
+<!-- Mettre une erreur au dessus du formulaire -->
+    <?php if(!empty($errors)): ?>
         <div class="alert alert-danger">
-            <?= $error ?>
+            Formulaire invalide.
+        </div>
+    <?php elseif($success): ?>
+        <div class="alert alert-success">
+            <?= $success ?>
         </div>
     <?php endif ?>
 
     <form action="" method="POST" class="form-group">
-        <input type="text" name="username" placeholder="Votre nom" class="form-control"><br>
-        <textarea name="message" cols="30" rows="3" class="form-control">Votre message</textarea><br>
+        <input type="text" name="username" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?> "><br>
+        <!-- Mettre une erreur en bas de chaque input -->
+        <?php if(isset($errors['username'])): ?>
+            <div class="invalid-feedback">
+                <?= $errors['username'] ?>
+            </div>
+        <?php endif ?>
+
+        <textarea name="message" cols="30" rows="3" class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?>" placeholder="Votre message"></textarea><br>
+        <!-- Mettre une erreur en bas de chaque input -->
+            <?php if(isset($errors['message'])): ?>
+                <div class="invalid-feedback">
+                    <?= $errors['message'] ?>
+                </div>
+            <?php endif ?>
+        
         <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
 
